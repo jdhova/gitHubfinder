a77f4b42da62d41bbe2dd78bdb955dce4494cf1d
Jude Okagu <juud0010@gmail.com>
2020-05-08 15:07:13 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Users from './components/user/Users';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
+
+        <Users />
       </div>
     );
   }
