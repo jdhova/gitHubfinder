5f05d33c00b3265970fd444ced0a5e82db7a0584
Jude Okagu <juud0010@gmail.com>
2020-05-10 15:17:46 +0200

    fecthing with api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/user/Users';
+import Axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await Axios.get('https://api.github.com/users');
+    this.setState({ users: res.data });
+    this.setState({ loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-
-        <Users />
+        <div className='container'>
+          <Users users={this.state.users} loading={this.state.loading} />
+        </div>
       </div>
     );
   }
